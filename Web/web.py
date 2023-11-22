a02e47b96021342758fc28247e7299f78194e218
SaazNeupane <saazneu789@gmail.com>
2023-11-22 18:47:38 -0500

    Design Changes

--- Web/web.py
+++ Web/web.py
@@ -1,60 +1,86 @@
 import streamlit as st
+import os
 
 
 def input_and_process():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    img_filename = "logo.png"
+    try:
+        st.image(img_filename, use_column_width=True)
+    except FileNotFoundError:
+        st.error(
+            "Image not found in the specified folder. Please provide the correct image filename."
+        )
+
     st.title("Pet Find Adoption")
 
     fields = {
-        "Image": "file",
-        "Type": "text",
-        "Name": "text",
-        "Age": "text",
-        "Breed1": "text",
-        "Breed2": "text",
-        "Color1": "text",
-        "Color2": "text",
-        "Color3": "text",
-        "MaturitySize": "text",
-        "FurLength": "text",
-        "Vaccinated": "text",
-        "Dewormed": "text",
-        "Sterilized": "text",
-        "Health": "text",
-        "Quantity": "text",
-        "Fee": "text",
-        "Description": "text",
-        "AdoptionSpeed": "text",
+        "Image": {"type": "file", "required": True},
+        "Type": {"type": "text", "required": True},
+        "Name": {"type": "text", "required": True},
+        "Age": {"type": "number", "required": True},
+        "Breed1": {"type": "text", "required": False},
+        "Breed2": {"type": "text", "required": False},
+        "Color1": {"type": "text", "required": False},
+        "Color2": {"type": "text", "required": False},
+        "Color3": {"type": "text", "required": False},
+        "MaturitySize": {"type": "text", "required": False},
+        "FurLength": {"type": "text", "required": False},
+        "Vaccinated": {"type": "text", "required": False},
+        "Dewormed": {"type": "text", "required": False},
+        "Sterilized": {"type": "text", "required": False},
+        "Health": {"type": "text", "required": False},
+        "Quantity": {"type": "number", "required": False},
+        "Fee": {"type": "number", "required": False},
+        "Description": {"type": "text", "required": False},
+        "AdoptionSpeed": {"type": "number", "required": False},
     }
 
     data = {}
 
-    for field, data_type in fields.items():
-        if data_type == "file":
+    for field, options in fields.items():
+        field_type = options["type"]
+        required = options["required"]
+
+        if field_type == "file":
             data[field] = st.file_uploader(
                 f"Choose {field.lower()}", type=["jpg", "jpeg", "png"]
             )
-        elif data_type == "text":
-            data[field] = st.text_input(f"Enter {field}*", "")
-
-        elif data_type == "number":
-            data[field] = st.number_input(f"Enter {field}", value=0)
+        elif field_type == "text":
+            data[field] = st.text_input(f"Enter {field}{'*' if required else ''}", "")
+        elif field_type == "number":
+            data[field] = st.number_input(
+                f"Enter {field}{'*' if required else ''}", value=0
+            )
 
     if st.button("Process Data"):
-        if (
-            fields["Image"] in data
-            and data["Image"] is None
-            or not all(data[field] for field in fields if field != "Image")
-        ):
-            st.warning("Please fill in all fields and upload an image. * is mandatory")
+        missing_required_fields = [
+            field
+            for field, options in fields.items()
+            if options["required"] and not data.get(field)
+        ]
+
+        if missing_required_fields:
+            st.warning("Please fill in all mandatory fields.")
+            for field in missing_required_fields:
+                st.markdown(
+                    f"<span style='color:red'>Required field: {field}</span>",
+                    unsafe_allow_html=True,
+                )
         else:
             st.title("Processed Data")
-            if "Image" in data:
+            if "Image" in data and data["Image"] is not None:
                 st.image(data["Image"], caption="Uploaded Image", use_column_width=True)
             for field in fields:
                 if field != "Image":
                     st.write(f"{field}: {data[field]}")
 
             with st.spinner("Processing data..."):
+                # Simulate processing delay
+                import time
+
+                time.sleep(3)
                 st.success("Data processed successfully!")
 
 
