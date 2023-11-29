6cc35d2b5121f75f0cf83c45d73ffc2f6f689ac7
Robert Thapa <wyieborn@gmail.com>
2023-11-29 18:29:55 -0500

    repush after merge fix

--- Src/train.py
+++ Src/train.py
@@ -9,11 +9,7 @@ def train_rf(df):
     cols_to_drop = ['AdoptionSpeed']
     X = df.drop(cols_to_drop, axis=1)
     y = df['AdoptionSpeed']
-<<<<<<< HEAD
     model = engine.run_rf(X,y, config.GRIDCV_OPTION, None)    
     return model
-=======
-    engine.run_rf(X,y, config.GRIDCV_OPTION, None)    
->>>>>>> 8bc949e6ac3eff69a1d451a718948b0774f58406
 
     
